5b0006544dbca9ffa890594f58e5335c80e67c64
tranthinh <tranthjnh288@gmail.com>
2023-08-20 11:43:19 +0700

    first commit

---
+++ lecture3/hello/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'hello/index.html')
+ 
+def greeting(request, name):
+    return render(request, 'hello/greeting.html', {'name': name.capitalize()})
---
+++ lecture3/tasks/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
+
+from django import forms
+
+tasks = []
+
+class NewTasksForm(forms.Form):
+    task = forms.CharField(label='New tasks')
+
+
+# Create your views here.
+def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html",{
+        'tasks': request.session['tasks'],
+    })
+
+def add(request):
+    if request.method == 'POST':
+        form = NewTasksForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            request.session['tasks'] += [task]
+            return HttpResponseRedirect(reverse('tasks:index'))
+        else:
+            return render(request, "tasks/add.html",{
+                'form': form
+            })
+    return render(request, "tasks/add.html",{
+        'form': NewTasksForm()
+    })
+
